26297dd923154bbbc049524a5303068aaa848807
Huseinaby <huseinabu01@gmail.com>
2025-11-05 23:27:17 +0800

    Implement show method in quizController to retrieve quiz by ID

--- app/Http/Controllers/quizController.php
+++ app/Http/Controllers/quizController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Http\Request;
 
 class quizController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $quiz = Quiz::with('options')->find($id);
+
+        if (!$quiz) {
+            return response()->json(['message' => 'Quiz not found'], 404);
+        }
+
+        return response()->json([
+            'message' => 'Quiz retrieved successfully',
+            'quiz' => $quiz
+        ]);
+    }    
 }
