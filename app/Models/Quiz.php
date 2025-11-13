08441044a69ad7be89164495266d7406936eaf72
Huseinaby <huseinabu01@gmail.com>
2025-11-13 23:06:02 +0800

    Restore Quiz model with updated fillable fields and relationships

---
+++ app/Models/Quiz.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'sub_category_id',
+        'created_by',
+    ];
+
+    public function subCategory()
+    {
+        return $this->belongsTo(QuizSubCategories::class, 'sub_category_id');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+}
