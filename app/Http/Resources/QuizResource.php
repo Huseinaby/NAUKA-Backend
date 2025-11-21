95b686b77d9fa442ddf43c10eb6a65546058d135
Huseinaby <huseinabu01@gmail.com>
2025-11-21 22:38:00 +0800

    Refactor QuizResource to customize the toArray method for improved resource transformation

--- app/Http/Resources/QuizResource.php
+++ app/Http/Resources/QuizResource.php
@@ -14,6 +14,15 @@ class QuizResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'sub_category_id' => $this->sub_category_id,
+            'quiz_text' => $this->quiz_text,
+            'quiz_image' => $this->quiz_image ? asset($this->quiz_image) : null,
+            'choices' => ChoiceResource::collection($this->whenLoaded('choices')),
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at'=> $this->updated_at?->format('Y-m-d H:i:s'),
+        ];
     }
 }
